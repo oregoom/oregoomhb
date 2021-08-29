c7514eeef35aee0c5d6a891bfabde25830e68441
nivar <nivar@DESKTOP-VSL3HC5>
2021-08-29 10:42:04 -0500

    Casi Listo AT y NT

--- templates/template-antiguo-testamento.php
+++ templates/template-antiguo-testamento.php
@@ -13,41 +13,38 @@ if(have_posts()){
 <div class="d-none d-lg-block container-fluid pb-0 mb-0">
     
     <nav aria-label="breadcrumb" class="container d-none d-lg-block">
+
         <ol class="breadcrumb pl-0 pr-0 mb-0" style="background: #ffffff; font-size: 13px;">
             <li class="breadcrumb-item"><a href="<?php echo esc_url(home_url()); ?>">Inicio</a></li>                                
             <li class="breadcrumb-item active text-dark" aria-current="page"><?php the_title(); ?></li>
         </ol>
+        
     </nav>
     
 </div>
-
-<div class="container bg-white pb-5">
     
-    <article class=""><?php
+<article class="container bg-white pb-5"><?php
 
-        //IMG destacada de POST
-        if( has_post_thumbnail() ) {
+    //IMG destacada de POST
+    if( has_post_thumbnail() ) {
 
-            the_post_thumbnail('full', array( 'class' => 'img-fluid rounded mb-5 ml-5 mr-5' )); 
+        the_post_thumbnail('full', array( 'class' => 'img-fluid mb-4 mb-lg-5 ml-lg-5 mr-lg-5' )); 
 
-        } ?>
-                
-        <h1 class="pb-4 text-center" style="font-family: 'Salsa'; font-size: 40px;"><strong><?php the_title(); ?></strong></h1>
+    } ?>
+            
+    <h1 class="pb-4 text-center" style="font-family: 'Salsa'; font-size: 40px;"><strong><?php the_title(); ?></strong></h1>
+    
+    <div class="pt-1 pt-lg-3 pb-5">
         
-        <div class="pt-3 pb-5">
-            
-            <?php the_content(); ?>
-            
-        </div>  
+        <?php the_content(); ?>
+        
+    </div>  
 
-    </article>
-    
-</div>
-
-    <?php
+</article> <?php
     
     endwhile;
-    wp_reset_postdata();    
+    wp_reset_postdata();
+
 } ?>
 
 
@@ -58,8 +55,4 @@ if(have_posts()){
  * ===============================
  * Pie de página 
  */
-get_footer();
-                
-// if ( has_post_thumbnail() ) { the_post_thumbnail(); }
-            
- 
+get_footer(); ?>
